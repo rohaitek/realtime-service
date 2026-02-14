a54b167ff3d8536051fe7d4d589df6292174bb56
root <root@srv1299285.hstgr.cloud>
2026-02-14 21:28:15 +0000

    init realtime service

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("cliente conectado");
+
+  socket.on("disconnect", () => {
+    console.log("cliente desconectado");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Realtime service running");
+});
+
+const PORT = process.env.PORT || 3002;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
